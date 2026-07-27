8b4cd2932003304ba5d83447528a9fb212361f60
mushettynitheesh-afk <mushettynitheesh@gmail.com>
2026-07-27 10:16:52 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import gui.LoginFrame;
+
+public class Main {
+    public static void main(String[] args){
+        new LoginFrame();
+    }
+}
---
+++ src/model/Admin.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Admin extends Person {
+    private int adminId;
+
+    public Admin() {
+    }
+
+    public Admin(int adminId, String name, int age) {
+        super(name, age);
+        this.adminId = adminId;
+    }
+    public int getAdminId(){return adminId;}
+    public void setAdminId(int adminId){this.adminId = adminId; }
+    public void display(){
+        super.display();
+        System.out.println("Admin ID :" + adminId);
+    }
+}
